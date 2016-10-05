c97e2a3fa8998be1954b811edd52e73a4456874c
gm42 <g.mazzotta@hotelspecials.nl>
2016-10-05 10:15:03 +0200

    Added support for search

---
+++ search.go
@@ -0,0 +1,69 @@
+package confluence
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type ContentResult struct {
+	Content               Content `json:"content"`
+	Title                 string  `json:"title"`
+	Excerpt               string  `json:"excerpt"`
+	URL                   string  `json:"url"`
+	ResultGlobalContainer struct {
+		Title      string `json:"title"`
+		DisplayURL string `json:"displayUrl"`
+	}
+	// breadcrumbs is ignored
+	EntityType           string `json:"entityType"`
+	IconCSSClass         string `json:"iconCssClass"`
+	LastModified         string `json:"lastModified"`
+	FriendlyLastModified string `json:"friendlyLastModified"`
+}
+
+type Results struct {
+	Results        []ContentResult `json:"results"`
+	Start          int             `json:"size"`
+	Limit          int             `json:"limit"`
+	Size           int             `json:"size"`
+	TotalSize      int             `json:"totalSize"`
+	CqlQuery       string          `json:"cqlQuery"`
+	SearchDuration int             `json:"SearchDuration"`
+	// links are ignored
+}
+
+func (w *Wiki) searchEndpoint() (*url.URL, error) {
+	return url.ParseRequestURI(w.endPoint.String() + "/search")
+}
+
+func (w *Wiki) Search(cql, cqlContext string, expand []string, limit int) (*Results, error) {
+	searchEndPoint, err := w.searchEndpoint()
+	if err != nil {
+		return nil, err
+	}
+	data := url.Values{}
+	data.Set("expand", strings.Join(expand, ","))
+	data.Set("cqlcontext", cqlContext)
+	data.Set("cql", cql)
+	searchEndPoint.RawQuery = data.Encode()
+
+	req, err := http.NewRequest("GET", searchEndPoint.String(), nil)
+	if err != nil {
+		return nil, err
+
+	}
+	res, err := w.sendRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var results Results
+	err = json.Unmarshal(res, &results)
+	if err != nil {
+		return nil, err
+	}
+
+	return &results, nil
+}
